4e8d3088d77fd48815a22916001eae56b71c441d
Anna Voyteshchuk <anna.voyteshchuk@gmail.com>
2026-01-04 18:38:08 +0500

    fix: Promos API url fixed

--- BookingService/Services/PromoService.cs
+++ BookingService/Services/PromoService.cs
@@ -15,7 +15,7 @@ namespace BookingService.Services
 
 		public async Task<decimal> ResolvePromoDiscount(string promoCode, string userId)
 		{
-			var code = await _rest.PostNullableRest<Promocode>($"/api/promocodes/{promoCode}/{userId}");
+			var code = await _rest.PostNullableRest<Promocode>($"/api/promos/validate?code={promoCode}&userId={userId}");
 
 			return code == null ? 0 : code.discountPercent;
 		}
